d641d6ae2c0240b66c932a02ed536efd9da1ec31
Albin CAUDERLIER <albin.cauderlier@gmail.com>
2023-11-19 23:09:27 +0100

    First commit with PHP and Dockerfile upgrades

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CodeIgniter4-Docker-Apache-PHP-MySQL
+
+Step 1 - https://avenir.ro/codeigniter-4-using-docker-apache-mysql/
+
+Step 2 - https://avenir.ro/codeigniter-4/xinstall-codeigniter-in-docker-containers/
+
+1. Cloner ce repository Git
+
+2. Dans la racine du repository, lancer les commandes :
+
+# Build des containers et lancement des serveurs
+$ docker-compose up -d 
+
+# Lister les containers up poour avoir leurs noms
+$ docker ps 
+
+# Entrer dans le container web 
+$ docker exec -it codeigniter4-docker-apache-php-mysql-web-1 bash
+
---
+++ docker-clean-all.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+docker ps -a
+
+docker-compose down
+
+#docker container ls -a
+#docker container rm ... ...
+
+#docker container ls -a --filter status=exited --filter status=created
+#docker container prune
+#docker container prune --filter "until=12h"
+
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
+
+#docker image ls
+#docker image prune
+docker image prune -a
+#docker image prune -a --filter "until=12h"
+
+#docker volume ls
+docker volume prune
+
+#docker network ls
+docker network prune
+#docker network prune -a --filter "until=12h"
+
+#docker system prune
+docker system prune --volumes
+
+# docker system clean (to test)
+
+docker ps -a
+
+
+# Composer
+rm -rf www/vendor
+rm www/composer.lock
+
